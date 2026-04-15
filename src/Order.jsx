eb714146f3a454a7df2f82d0dea528fc889fdd51
djwirz <djwirz@gmail.com>
2026-04-15 13:25:16 -0500

    Custom hook and a component for pizza of the day

--- src/Order.jsx
+++ src/Order.jsx
@@ -23,7 +23,7 @@ export default function Order() {
   }, []);
 
   async function fetchPizzaTypes() {
-    await new Promise((resolve) => setTimeout(resolve, 3000)); // remove this later, just to show you the loading state
+    // await new Promise((resolve) => setTimeout(resolve, 3000)); // remove this later, just to show you the loading state
 
     const pizzasRes = await fetch("/api/pizzas");
     const pizzasJson = await pizzasRes.json();
